5006064e259a437a11b443c47e23815ecfe3019c
noorulhadi324-jpg <noorulhadi324@gmail.com>
2026-09-12 21:41:59 +0500

    Add Riverpod communication controller

---
+++ lib/features/communication/presentation/controllers/communication_controller.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../../../core/network/supabase_client.dart';
+import '../../data/repositories/communication_repository_impl.dart';
+
+final communicationRepositoryProvider = Provider<CommunicationRepositoryImpl>((ref) {
+  return CommunicationRepositoryImpl(client: SupabaseConfig.client);
+});
+
+final communicationSchoolUsersProvider = FutureProvider.autoDispose<List<Map<String, dynamic>>>((ref) async {
+  final schoolId = await ref.watch(schoolIdProvider.future);
+  if (schoolId == null) throw Exception('No school linked to your account.');
+  return ref.read(communicationRepositoryProvider).getSchoolUsers(schoolId);
+});
