ffbd494d9cfbecd2e179b33d1dad2759fa4bb597
noorulhadi324-jpg <noorulhadi324@gmail.com>
2026-09-12 21:42:35 +0500

    Complete send-message use case

--- lib/features/communication/domain/usecases/send_message.dart
+++ lib/features/communication/domain/usecases/send_message.dart
@@ -1,2 +1,19 @@
-// Placeholder
-class SendMessage {}
+import '../repositories/communication_repository.dart';
+
+class SendMessage {
+  final CommunicationRepository repository;
+
+  const SendMessage(this.repository);
+
+  Future<void> call({
+    required int threadId,
+    required String senderId,
+    required String body,
+  }) {
+    return repository.sendMessage(
+      threadId: threadId,
+      senderId: senderId,
+      body: body,
+    );
+  }
+}
