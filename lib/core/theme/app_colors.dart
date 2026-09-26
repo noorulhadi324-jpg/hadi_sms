98e73b736beff98506ec636924e0efc9c2ec6524
Noor ul hadi <273912261+noorulhadi324-jpg@users.noreply.github.com>
2026-09-26 14:07:06 +0500

    style: improve light backgrounds and readable foreground contrast

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,11 +1,1 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const background = Color(0xFFF4F7FB);
-  static const surface = Color(0xFFFFFFFF);
-  static const primary = Color(0xFF1B4D8F);
-  static const secondary = Color(0xFF2FA4FF);
-  static const textPrimary = Color(0xFF17202A);
-  static const textSecondary = Color(0xFF6B7280);
-  static const error = Color(0xFFD32F2F);
-}
+export '../constants/app_colors.dart';
